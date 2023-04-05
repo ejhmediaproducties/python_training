737222603c2106ea20e641502cc908b6b955ba14
EJH Media Producties <info@ejhmediaproducties.nl>
2023-04-05 07:58:51 +0200

    Create Oplossingen_opdrachten_variabelen.py

---
+++ Oplossingen_opdrachten_variabelen.py
@@ -0,0 +1,27 @@
+'''
+OPDRACHT 1
+'''
+
+# Voorbeeldopdracht voor variabelen en berekeningen
+nummer1 = int(input("Voer een getal in: "))
+nummer2 = int(input("Voer nog een getal in: "))
+
+som = nummer1 + nummer2
+verschil = nummer1 - nummer2
+product = nummer1 * nummer2
+quotient = nummer1 / nummer2
+
+print("De som is:", som)
+print("Het verschil is:", verschil)
+print("Het product is:", product)
+print("Het quotient is:", quotient)
+
+
+'''
+OPDRACHT 2
+'''
+# Voorbeeldopdracht voor variabelen en strings
+naam = input("Wat is je naam? ")
+leeftijd = input("Hoe oud ben je? ")
+
+print("Hallo, mijn naam is", naam, "en ik ben", leeftijd, "jaar oud.")
